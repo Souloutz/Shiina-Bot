209b9436633f9540198382701528097e96415ac8
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 15:56:39 -0400

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,22 +1,17 @@
 const { prefix } = require('../config.json');
-const fs = require("fs");
+const { readdirSync } = require("fs");
 
 module.exports = (bot) => {
-    fs.readdir("./commands/", (err, files) => {
-
-        if(err) console.log(err)
-    
-        let jsfile = files.filter(f => f.split(".").pop() === "js") 
-        if(jsfile.length <= 0) {
-             return console.log("[LOGS] Couldn't Find Commands!");
-        }
-    
-        jsfile.forEach((f, i) => {
-            let pull = require(`./commands/${f}`);
-            bot.commands.set(pull.name, pull);  
-            pull.aliases.forEach(alias => {
-                bot.aliases.set(alias, pull.name)
-            });
-        });
-    });
+    readdirSync("./commands/").map(dir => {
+       const commands = readdirSync(`./commands/${dir}/`).map(cmd=>{
+           let pull = require(`../commands/${dir}/${cmd}`)
+           console.log(`Loaded command ${prefix}${pull.name}`)
+           bot.commands.set(pull.name, pull)
+           if(cmd.aliases){
+               cmd.aliases.forEach(alias =>{
+                   bot.aliases.set(alias, pull.name)
+               })
+           }
+       })
+    })
 }
