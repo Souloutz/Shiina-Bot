e8eb2ed06e4f2aa94a9ac155ef93c42c7de51166
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 16:04:00 -0400

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -6,15 +6,14 @@ module.exports = (bot) => {
         const commands = readdirSync(`./commands/${dir}/`).map(cmd=>{
             let pull = require(`../commands/${dir}/${cmd}`)
             console.log(`Loaded command ${prefix}${pull.name}`)
-            bot.commands.set(pull.name, pull)
-           
-            jsfile.forEach((f, i) => {
+            bot.commands.set(pull.name,pull)
+            if(cmd.aliases){
                 let pull = require(`./commands/${f}`);
                 bot.commands.set(pull.config.name, pull);  
                 pull.config.aliases.forEach(alias => {
                     bot.aliases.set(alias, pull.config.name)
-                });
-            });
+                })
+            }
         })
     })
 }
