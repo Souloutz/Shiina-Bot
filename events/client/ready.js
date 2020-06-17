85e17d4cff433e6a1b42070d104dc95fd9cae09e
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 12:14:06 -0400

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = bot => {
-    bot.user.setActivity('s!help', {type: 'PLAYING'});
+    bot.user.setActivity('s!help | Finally hosted 24/7', {type: 'PLAYING'});
     console.log(`${bot.user.tag} is online!`)
 };
