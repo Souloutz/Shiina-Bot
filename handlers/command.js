16b358ea9528e5da942270aacdd75c07002ff937
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 16:16:44 -0400

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -7,13 +7,11 @@ module.exports = (bot) => {
             let pull = require(`../commands/${dir}/${cmd}`)
             console.log(`Loaded command ${prefix}${pull.name}`)
             bot.commands.set(pull.name,pull)
-            if(cmd.aliases){
-                let pull = require(`./commands/${dir}/${f}`);
-                bot.commands.set(pull.name, pull);  
-                pull.aliases.forEach(alias => {
-                    bot.aliases.set(alias, pull.name)
-                })
-            }
+            let pull = require(`./commands/${dir}/${f}`);
+            bot.commands.set(pull.name, pull);  
+            pull.aliases.forEach(alias => {
+                bot.aliases.set(alias, pull.name)
+            })          
         })
     })
 }
