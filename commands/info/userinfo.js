3bed223ec0473d6bded061971fcd7a5c3c2505e3
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 23:00:24 -0400

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -10,28 +10,11 @@ module.exports = {
     usage: '[user]',
     aliases: ["user"],
     run: async(bot, message, args) => {
-        const flags = {
-            DISCORD_EMPLOYEE: 'Discord Employee',
-            DISCORD_PARTNER: 'Discord Partner',
-            BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
-            BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
-            HYPESQUAD_EVENTS: 'HypeSquad Events',
-            HOUSE_BRAVERY: 'House of Bravery',
-            HOUSE_BRILLIANCE: 'House of Brilliance',
-            HOUSE_BALANCE: 'House of Balance',
-            EARLY_SUPPORTER: 'Early Supporter',
-            TEAM_USER: 'Team User',
-            SYSTEM: 'System',
-            VERIFIED_BOT: 'Verified Bot',
-            VERIFIED_DEVELOPER: 'Verified Bot Developer'
-        };
-        
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         const roles = member.roles.cache
             .sort((a, b) => b.position - a.position)
             .map(role => role.toString())
             .slice(0, -1);
-        const userflags = member.user.flags.toArray();
         const Info = new MessageEmbed()
             .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512}))
             .setColor(colors.peach)
@@ -39,7 +22,6 @@ module.exports = {
                 `**Username:** ${member.user.username}`,
                 `**Discriminator:** ${member.user.discriminator}`,
                 `**ID:** ${member.id}`,
-                `**Flags:** ${userflags.length ? userflags.map(flag => flags[flags]).join(', ') : 'None'}`,
                 `**Avatar:** [Link to Avatar](${member.user.displayAvatarURL({ dynamic: true })})`,
                 `**Time Created:** ${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`,
                 `**Status:** ${member.user.presence.status}`,
