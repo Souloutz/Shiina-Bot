f0753eef5f818fe14e291012af9df675007d6b1d
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-18 19:33:47 -0400

    1.0.1
    -Fixes to dm command

--- commands/fun/dm.js
+++ commands/fun/dm.js
@@ -11,17 +11,19 @@ module.exports = {
 
         if(!args[0]) return message.channel.send('Please specify a user to message!');
         const member = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || await bot.users.fetch(args[0]);
-        if(!member) return message.channel.send('That is not a user in the server! Try again!');
+        if(member !== message.guild.members) return message.channel.send('That is not a valid user in the server! Try again!');
+        if(member === member.user.bot) return message.channel.send('You can not send messages to bots!');
+
 
         if(!args[1]) return message.channel.send('Please supply a message!');
         const dmmessage = args.slice(1, ).join(" ");
         if(!dmmessage) return message.channel.send('Please supply a message!');
 
         member.send(`${dmmessage}`);
-        const Delivered = await message.channel.send('Message Sent!')
+        message.channel.send('Message Sent!')
 
         async function Wait() {
-            Delivered.delete();
+            message.channel.bulkDelete(2);
         };
 
         setTimeout(Wait, 1500);
