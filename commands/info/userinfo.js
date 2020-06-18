e2949015df6ec4e3c5d9692bc04bdfa5bdc70ad9
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 22:58:12 -0400

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -3,22 +3,6 @@ const colors = require("../../colors.json");
 const moment = require('moment');
 require('moment-duration-format');
 
-const flags = {
-	DISCORD_EMPLOYEE: 'Discord Employee',
-	DISCORD_PARTNER: 'Discord Partner',
-	BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
-	BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
-	HYPESQUAD_EVENTS: 'HypeSquad Events',
-	HOUSE_BRAVERY: 'House of Bravery',
-	HOUSE_BRILLIANCE: 'House of Brilliance',
-	HOUSE_BALANCE: 'House of Balance',
-	EARLY_SUPPORTER: 'Early Supporter',
-	TEAM_USER: 'Team User',
-	SYSTEM: 'System',
-	VERIFIED_BOT: 'Verified Bot',
-	VERIFIED_DEVELOPER: 'Verified Bot Developer'
-};
-
 module.exports = {
     name: 'userinfo',
     category: 'info',
@@ -26,6 +10,22 @@ module.exports = {
     usage: '[user]',
     aliases: ["user"],
     run: async(bot, message, args) => {
+        const flags = {
+            DISCORD_EMPLOYEE: 'Discord Employee',
+            DISCORD_PARTNER: 'Discord Partner',
+            BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
+            BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
+            HYPESQUAD_EVENTS: 'HypeSquad Events',
+            HOUSE_BRAVERY: 'House of Bravery',
+            HOUSE_BRILLIANCE: 'House of Brilliance',
+            HOUSE_BALANCE: 'House of Balance',
+            EARLY_SUPPORTER: 'Early Supporter',
+            TEAM_USER: 'Team User',
+            SYSTEM: 'System',
+            VERIFIED_BOT: 'Verified Bot',
+            VERIFIED_DEVELOPER: 'Verified Bot Developer'
+        };
+        
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         const roles = member.roles.cache
             .sort((a, b) => b.position - a.position)
