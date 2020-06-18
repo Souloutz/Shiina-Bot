07bf2c2388260a0f6682b69d2b1818982ec43dc2
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-17 23:35:25 -0400

    1.1.0

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -9,20 +9,50 @@ module.exports = {
     description: 'Displays server info',
     aliases: ['server'],
     run: async(bot, message, args) => {
-        const Server = new MessageEmbed()
-            .setColor(colors.orange)
-            .setThumbnail(message.guild.iconURL())
-            .setAuthor(message.guild.name, message.guild.iconURL())
-            .addField('Name', message.guild.name, true)
-            .addField('Owner', message.guild.owner, true)
-            .addField('ID', message.guild.id, false)
-            .addField('Members', message.guild.memberCount, true)
-            .addField('Roles', message.guild.roles.cache.size, true)
-            .addField('Bots', message.guild.members.bots.cache.size, true)
-            .addField('Verification Level', message.guild.verificationLevel, false)
-            .addField('Created', moment.utc(message.guild.createdAt).format("ddd, MMMM Do, YYYY, hh:mm:ss A"), true)
-            .setFooter(`Shiina | Created by Souloutz#0038`)
-        message.channel.send(Server);
+        const roles = message.guild.roles.cache
+            .sort((a, b) => b.position - a.position)
+            .map(role => role.toString());
+        const members = message.guild.members.cache
+        const channels = message.guild.channels.cache
+        const emojis = message.guild.emojis.cache
+        
+        const Info = new MessageEmbed()
+            .setDescription(`**Information for __${message.guild.name}__**`)
+            .setColor(colors.peach)
+            .setThumbnail(message.guild.iconURL({ dynamic: true }))
+            .addField('General', [
+                `**Name:** ${message.guild.name}`,
+                `**ID:** ${message.guild.id}`,
+                `**Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+                `**Region:** ${regions[message.guild.regions]}`,
+                `**Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+                `**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+                `**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+                `**Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+                `\u200b`
+            ])
+            .addField('Statistics', [
+                `**Role Count:** ${roles.length}`,
+                `**Emoji Count:** ${emojis.size}`,
+                `**Member Count:** ${message.guild.memberCount}`,
+                `**Humans:** ${members.filter(member => !member.user.bot).size}`,
+                `**Bots:** ${members.filter(member => member.user.bot).size}`,
+                `**Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+                `**Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
+                `**Boost Count:** ${message.guild.permiumSubscriptionCount || '0'}`,
+                `\u200b`
+            ])
+            .addField('Presence', [
+                `**Online:** ${members.filter(member => member.presence.status === 'online').size}`,
+                `**Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
+                `**Do Not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
+                `**Offline:** ${members.filter(member => member.presence.status === 'offline').size}`
+                `\u200b`
+            ])
+            .addField(`Roles [${roles.length} - 1]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None')
+            .setFooter('Shiina | Developed by Souloutz#0038')
+            .setTimestamp();
+        return message.channel.send(Info);
     }
 };
 
