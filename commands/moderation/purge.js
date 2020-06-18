f8d26cfee27f10a63c493de8fc75887b7d1eaa0e
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-18 15:41:44 -0400

    Hotfix 1.0.0
    -Fix purge command

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -3,6 +3,7 @@ module.exports = {
     category: 'moderation',
     description: 'Purge messages',
     usage: '<number>',
+    timeout: 2500,
     run: async(bot, message, args) => {
         const permissions = message.member.hasPermission("MANAGE_MESSAGES");
         if(!permissions) return message.channel.send('You do not have permission to execute this command!');
@@ -15,9 +16,9 @@ module.exports = {
 
         if (amount > 100) return message.channel.send('You can\'t delete more than 100 messages at once!'); 
         if (amount < 1) return message.channel.send('You have to delete at least 1 message!');
-        amount++;
 
         await message.channel.messages.fetch({ limit : amount }).then(messages => { // Fetches the messages
+            message.channel.bulkDelete(1);
             message.channel.bulkDelete(messages);
         });
     }
