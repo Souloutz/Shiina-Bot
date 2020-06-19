fe138eb89035ce2f557c91c98dc3afd14a2d65a2
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-18 21:21:50 -0400

    Update dm.js

--- commands/fun/dm.js
+++ commands/fun/dm.js
@@ -12,8 +12,8 @@ module.exports = {
         const members = message.guild.members.cache
         const bots = members.filter(member =>  member.user.bot)
         const user = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || await bot.users.fetch(args[0]);
-        if(!user) return message.channel.send('That is not a user in the server! Try again!');
         if(user === bots) return message.channel.send('You can not send a message to a bot!');
+        if(!user) return message.channel.send('That is not a user in the server! Try again!');
         
         if(!args[1]) return message.channel.send('Please supply a message!');
         const dmmessage = args.slice(1, ).join(" ");
