81e11baee7ab238e3835f81f5323e31472c73dd7
Souloutz <67064694+Souloutz@users.noreply.github.com>
2020-06-19 14:22:26 -0400

    Update 1.2.0
    -Removed display roles
    -Cleaned categories

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -44,7 +44,6 @@ module.exports = {
         const members = message.guild.members.cache;
         const channels = message.guild.channels.cache;
         const emojis = message.guild.emojis.cache;
-        const displayRole = roles.length < 10 ? roles.join(', ') : roles.length > 10 ? (roles) : 'None';
 
         const Info = new MessageEmbed()
             .setDescription(`**Information for __${message.guild.name}__**`)
@@ -73,7 +72,7 @@ module.exports = {
                 `**Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
                 `**Voice Channels:** ${channels.filter(channel => channel.type === 'voice').size}`,
                 `**Emoji Count:** ${emojis.size}`,
-                `**Roles: [${roles.length}]:** ${displayRole.toString()}`,
+                `**Roles:** ${roles.length}`,
                 `\u200b`
             ])
             .setFooter('Shiina | Developed by Souloutz#0038')
@@ -87,4 +86,7 @@ module.exports = {
 `**Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
 `**Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
  `**Boost Count:** ${message.guild.permiumSubscriptionCount || '0'}`,
+
+const displayRole = roles.length < 10 ? roles.join(', ') : roles.length > 10 ? (roles) : 'None';
+ `**Roles: [${roles.length}]:** ${displayRole.toString()}`,
 */
